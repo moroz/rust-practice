dc70c2cca7f5869581036844e52c7fbfc697751d
Karol Moroz <k.j.moroz@gmail.com>
2022-07-01 23:40:07 +0200

    Parse CLI args using clap

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,13 +1,19 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::path::{Path, PathBuf};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
+    println!("{} {}", args.pattern, args.path.as_path().display());
 }
