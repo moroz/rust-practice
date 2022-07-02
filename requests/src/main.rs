8fe5b2c05569a750fad6b8b9ccb122d409a522e2
Karol Moroz <k.j.moroz@gmail.com>
2022-07-02 21:54:54 +0200

    Extract text from response

--- requests/src/main.rs
+++ requests/src/main.rs
@@ -3,20 +3,19 @@ use std::io;
 
 const SEARCH_URL: &str = "https://api.spotify.com/v1/search";
 
+async fn search() -> String {
+    return reqwest::get(SEARCH_URL)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let response = reqwest::get(SEARCH_URL).await.unwrap();
-    match response.status() {
-        StatusCode::OK => {
-            println!("Success! {:?}", response);
-        }
-        StatusCode::UNAUTHORIZED => {
-            println!("Need to grab a new token");
-        }
-        _ => {
-            panic!("Uh oh! Something unexpected happened!");
-        }
-    }
+    let result = search().await;
+    println!("{}", result);
 
     Ok(())
 }
