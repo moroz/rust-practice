25eab7dbe30984b8f2d3f7e8ddcedceb15472a06
Karol Moroz <k.j.moroz@gmail.com>
2022-07-02 22:15:31 +0200

    Setting headers, query string

--- requests/src/main.rs
+++ requests/src/main.rs
@@ -1,10 +1,31 @@
-use reqwest::{self, StatusCode};
+use reqwest::header::HeaderMap;
+use reqwest::header::*;
+use reqwest::{self};
 use std::io;
+
+fn get_api_token() -> String {
+    std::env::var("SPOTIFY_OAUTH_TOKEN")
+        .expect("Environment variable SPOTIFY_OAUTH_TOKEN is not set!")
+}
+
+fn build_headers() -> HeaderMap {
+    let token = get_api_token();
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, format!("Bearer {}", token).parse().unwrap());
+    headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+    headers
+}
 
 const SEARCH_URL: &str = "https://api.spotify.com/v1/search";
 
-async fn search() -> String {
-    return reqwest::get(SEARCH_URL)
+async fn search(query: &str) -> String {
+    let client = reqwest::Client::new();
+    return client
+        .get(SEARCH_URL)
+        .query(&[("query", query), ("type", "track,artist")])
+        .headers(build_headers())
+        .send()
         .await
         .unwrap()
         .text()
@@ -14,7 +35,7 @@ async fn search() -> String {
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let result = search().await;
+    let result = search("Little Simz").await;
     println!("{}", result);
 
     Ok(())
