88f8f1f8a51bc2ced4032a40527ee21c52703d85
Karol Moroz <k.j.moroz@gmail.com>
2022-07-02 21:52:30 +0200

    Naive status code matching with match

--- requests/src/main.rs
+++ requests/src/main.rs
@@ -1,7 +1,22 @@
-use reqwest;
+use reqwest::{self, StatusCode};
+use std::io;
+
+const SEARCH_URL: &str = "https://api.spotify.com/v1/search";
 
 #[tokio::main]
-async fn main() {
-    let result = reqwest::get("https://moroz.dev").await;
-    println!("{:?}", result);
+async fn main() -> io::Result<()> {
+    let response = reqwest::get(SEARCH_URL).await.unwrap();
+    match response.status() {
+        StatusCode::OK => {
+            println!("Success! {:?}", response);
+        }
+        StatusCode::UNAUTHORIZED => {
+            println!("Need to grab a new token");
+        }
+        _ => {
+            panic!("Uh oh! Something unexpected happened!");
+        }
+    }
+
+    Ok(())
 }
